280b45358b9e14c355a8f1f693177760f0aa792d
Khang Le <levuminhkhang@gmail.com>
2018-02-09 00:01:06 +0700

    remove

--- src/openStream.js
+++
@@ -1,9 +0,0 @@
-function openStream(callback) {
-    navigator.mediaDevices.getUserMedia({audio: false, video: true})
-        .then(stream => {
-        callback(stream);
-})
-.catch(error => console.log(error));
-}
-
-module.exports = openStream;
--- src/playVideo.js
+++
@@ -1,8 +0,0 @@
-function playVideo(stream,video) {
-    video.srcObject = stream;
-    video.onLoadedMetadata = function(){
-        video.play();
-    };
-}
-
-module.exports = playVideo;
